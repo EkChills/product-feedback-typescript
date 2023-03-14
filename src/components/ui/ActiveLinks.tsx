7e6da56fcaa1102a02c8b2eafb828b1e026e3a41
Ekene <ekeneezeifeomasam27@gmail.com>
2023-03-14 11:43:24 +0100

    added types to styled component

--- src/components/ui/ActiveLinks.tsx
+++ src/components/ui/ActiveLinks.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ProductRequest } from "../../types/storeTypes";
+// import {} from 'styled-components/cssprop'
 
-interface Props extends React.ComponentPropsWithoutRef<'span'> {
+interface Props extends React.ComponentPropsWithRef<'span'> {
   linkArray: ProductRequest[];
   text: string;
   className?:string
@@ -16,13 +17,18 @@ const ActiveLinks = ({linkArray,text,className,tab,...rest}:Props) => {
     else setActive(false)
   }, [text,tab])
   return (
-    <Wrapper active={active} text={text} {...rest} className={`text-[.8125rem] link-active cursor-pointer font-bold ${tab === text ? 'text-[#3A4374]' : ' text-[hsl(231,33%,75%)]'} ${className}`}>
+    <Wrapper active ={active} text={text} {...rest} className={`text-[.8125rem] link-active cursor-pointer font-bold ${tab === text ? 'text-[#3A4374]' : ' text-[hsl(231,33%,75%)]'} ${className}`}>
       {text} ({linkArray.length})
     </Wrapper>
   );
 };
 
-const Wrapper = styled.span`
+interface StyledProps {
+  active:boolean;
+  text:string;
+}
+
+const Wrapper = styled.span<StyledProps>`
 
     position: relative;
 
@@ -33,7 +39,7 @@ const Wrapper = styled.span`
     right: -0.2rem;
     top: 2.5rem;
     height: 4px;
-    background-color: ${props => props.active ? props.text == 'in-progress' ? '#AD1FEA' : props.text === 'planned' ? '#F49F85' : '#62BCFA' : 'transparent'};
+    background-color: ${props  => props.active  ? props.text == 'in-progress' ? '#AD1FEA' : props.text === 'planned' ? '#F49F85' : '#62BCFA' : 'transparent'};
   }
 `;
 
