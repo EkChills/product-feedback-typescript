b7c6acb3f49c86168d53423c332a3a67e8f5c56e
Ekene <ekeneezeifeomasam27@gmail.com>
2023-03-14 23:18:49 +0100

    css changes

--- src/pages/Roadmap.tsx
+++ src/pages/Roadmap.tsx
@@ -20,21 +20,21 @@ const Roadmap = () => {
         <RoadmapPageLinks tab={tab} handleTabChange={handleTabChange} />
         <div className="px-[1.5rem] flex-col space-y-[1rem]">
           {tab === 'planned' ? <>
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 py-[1.5rem]">
             <h3 className="text-[1.125rem] font-bold text-[#3A4374] capitalize">planned ({planned.length})</h3>
-            <p className="font-[.8125rem] font-[400] text-[#647196]">Ideas prioritized for research</p>
+            <p className="text-[.8125rem] font-[400] text-[#647196]">Ideas prioritized for research</p>
           </div>
             {planned.map((plan) => <Suggestion key={plan.id} tabName={tab} {...plan} />)}
           </> : tab == 'in-progress' ? <>
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 py-[1.5rem] ">
             <h3 className="text-[1.125rem] font-bold text-[#3A4374] capitalize">in-Progress ({inProgress.length})</h3>
-            <p className="font-[.8125rem] font-[400] text-[#647196]">Features currently being developed</p>
+            <p className="text-[.8125rem] font-[400] text-[#647196]">Features currently being developed</p>
           </div>
             {inProgress.map((feedback) => <Suggestion key={feedback.id} {...feedback} tabName={tab} />)}
           </> : <>
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 py-[1.5rem]">
             <h3 className="text-[1.125rem] font-bold text-[#3A4374] capitalize">live ({live.length})</h3>
-            <p className="font-[.8125rem] font-[400] text-[#647196]">Released features</p>
+            <p className="text-[.8125rem] font-[400] text-[#647196]">Released features</p>
           </div>
             {live.map((feedback) => <Suggestion key={feedback.id} {...feedback} tabName={tab} />)}
           </>}
